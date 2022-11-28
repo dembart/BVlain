131a70c28d355caf74948164f8a66be07cbea149
Artem <artemdembitskiy@192.168.1.8>
2022-11-28 21:26:31 +0300

    setup file

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setuptools.setup(
      classifiers=[
          "Programming Language :: Python :: 3.8",
          "Programming Language :: Python :: 3.9",
+         "Programming Language :: Python :: 3.10",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
